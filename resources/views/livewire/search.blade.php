3704fdedd4096afc5a79dff964ba574e20350fb6
tejas._.gk <66897829+tejas-gk@users.noreply.github.com>
2022-05-31 07:19:36 +0530

    search placeholder added

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,6 +1,6 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-    <input type="text" wire:model="searchTerm" />   
+    <input type="text" wire:model="searchTerm" placeholder="search... />   
     <button wire:click="search">Search</button>
     <div>
         <ul>
