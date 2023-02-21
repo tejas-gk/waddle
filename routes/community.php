133293455fa1742370421290e3698f356a6fdbfb
tejas._.gk <tejasgk250@gmail.com>
2023-02-21 17:49:55 +0530

    Update community.php

--- routes/community.php
+++ routes/community.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\community\CommunityController;
 use App\Http\Controllers\community\postsController;
 
-// Route::view('/c','community.community');
+
+ Route::view('/c','community.community');
 Route::view('/{community:community_name}',[CommunityController::class,'community']);
 Route::post('/create-new-community',[CommunityController::class,'createNewCommunity'])->middleware('auth');
 Route::resource('community', postsController::class);
